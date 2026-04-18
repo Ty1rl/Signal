94d90d419e4314aa1b28986b35ebce1769d0a446
Ty1rl <Ty1rl@live.com>
2026-04-18 11:06:16 -0500

    Add isometric prototype assets and level

--- menus/title_menu.gd
+++ menus/title_menu.gd
@@ -1,7 +1,9 @@
 extends Control
 
+const TEST_LEVEL: PackedScene = preload("uid://btp7inee50k1p")
+
 func _on_play_button_pressed() -> void:
-	pass
+	get_tree().change_scene_to_packed(TEST_LEVEL)
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
