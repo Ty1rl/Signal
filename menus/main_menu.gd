e6ecc3f6cde9f7101dfcc874459af9623331dd59
Ty1rl <Ty1rl@live.com>
2026-04-19 00:31:43 -0500

    Integrate level select into main scene kinda

--- menus/main_menu.gd
+++ menus/main_menu.gd
@@ -2,6 +2,11 @@ extends Control
 
 @onready var title_menu: Control = $TitleMenu
 @onready var options_menu: Control = $OptionsMenu
+@onready var level_select: Control = $LevelSelect
+
+func _on_play_button_pressed() -> void:
+	title_menu.hide()
+	level_select.show()
 
 func _on_options_button_pressed() -> void:
 	title_menu.hide()
--- menus/title_menu.gd
+++ menus/title_menu.gd
@@ -1,9 +1,4 @@
 extends Control
 
-const TEST_LEVEL: PackedScene = preload("uid://btp7inee50k1p")
-
-func _on_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://menus/level_select.tscn")
-
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
