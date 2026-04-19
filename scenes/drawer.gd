791f7b17447abd200cbd9b96e79acf81f82326f7
mattlongname <000@aan.aleeas.com>
2026-04-19 16:09:20 -0500

    testing collision poly

--- scenes/drawer.gd
+++ scenes/drawer.gd
@@ -5,11 +5,13 @@ signal ability_chosen(ability: String)
 @onready var wide_button: Button = $Buttons/WideButton
 @onready var pulse_button: Button = $Buttons/PulseButton
 @onready var skip_button: Button = $Buttons/SkipButton
+@onready var off_button: Button = $Buttons/OffButton
 
 func _ready() -> void:
 	wide_button.pressed.connect(func(): ability_chosen.emit("Wide"))
 	pulse_button.pressed.connect(func(): ability_chosen.emit("Pulse"))
 	skip_button.pressed.connect(func(): ability_chosen.emit("Skip"))
+	off_button.pressed.connect(func(): ability_chosen.emit("Off")) 
 	hide()
 
 func show_near(screen_pos: Vector2, viewport_size: Vector2) -> void:
